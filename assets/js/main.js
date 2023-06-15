69b567a8a84892705b43b59c4b5e2cb26730acb7
Sravan <sravan@docme.cloud>
2023-06-15 17:47:33 +0530

    added student and bg image in hero

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,18 @@
-$(function () {
-     gsap.registerPlugin(ScrollTrigger);
+// $(function () {
+     // gsap.registerPlugin(ScrollTrigger);
 
-     // GSAP Animation
-     // const tl = gsap.timeline();
-     // smooth scrolling container
+     // // GSAP Animation
+     // // const tl = gsap.timeline();
+     // // smooth scrolling container
      // const smoother = ScrollSmoother.create({
      //      wrapper: ".body",
      //      content: ".scroll_smoother",
      //      smooth: 0.8,
 
      // });
-     // /* After Adding New Content to DOM */
+     // // /* After Adding New Content to DOM */
      // smoother.refresh();
-});
+// });
 
 document.querySelector('.button').onmousemove = function (e) {
 
